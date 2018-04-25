2b89590dd2fb728901fdc1ea7052037ec2b3abbb
lzakrzewski <lucaszz111@gmail.com>
2018-04-25 08:42:28 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import promise from 'redux-promise';
+import reducers from './reducers';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+    <div>
+    <Switch>
+        <Route path="/" component={Calendar} />
+    </Switch>
+    </div>
+        </BrowserRouter>
+    </Provider>
+, document.querySelector('.container'));
