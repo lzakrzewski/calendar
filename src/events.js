3d9d9859cf24b3dab5ecd016e7a7dbd5b8993dc1
lzakrzewski <lucaszz111@gmail.com>
2018-05-23 00:18:35 +0100

    sorting events

--- src/events.js
+++ src/events.js
@@ -4,8 +4,9 @@ import _ from 'lodash';
 export const dateEvents = (date, events) => {
     const day = moment(date).startOf('day');
 
-    return _.filter(
-        events,
-        (event) => moment(event.date).startOf('day').isSame(day)
+    return _.orderBy(
+        _.filter(events, (event) => moment(event.start).startOf('day').isSame(day)),
+        ['start'],
+        ['asc']
     );
 };
