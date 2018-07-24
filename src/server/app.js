7e7156d1671b6bde52514a4357a22877561999b1
lzakrzewski <lucaszz111@gmail.com>
2018-07-24 23:02:31 +0100

    events, users mocked endpoints

--- src/server/app.js
+++ src/server/app.js
@@ -1,19 +1,52 @@
 import express from 'express';
+import uuid4 from 'uuid/v4';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/events', (req, res) => {
-    const response = [
-            {
-                id: 'event-id-1',
-                start: '2017-01-01',
-                end: '2017-01-02',
-                event: "Quick event 1",
-                allDay: false
-            }
-        ];
+app.get('/events', (request, response) => {
+    const userId = request.query.userId;
 
-    res.json(response);
+    if (userId === 'unknown') {
+        return response.status(403).json({error: 'Bad credentials.'});
+    }
+
+    return response.json([
+        {
+            id: 'event-id-1',
+            start: '2017-01-01',
+            end: '2017-01-02',
+            event: "Quick event 1",
+            allDay: false
+        },
+        {
+            id: 'event-id-2',
+            start: '2017-01-01',
+            end: '2017-01-02',
+            event: "Quick event 2",
+            allDay: true
+        }
+    ]);
+});
+
+app.post('/events', (request, response) => {
+    const userId = request.query.userId;
+    const event = request.body;
+
+    if (userId === 'unknown') {
+        return response.status(403).json({error: 'Bad credentials.'});
+    }
+
+    if (!event.start || !event.event) {
+        return response.status(400).json({error: 'Bad request'});
+    }
+
+    return response.status(201).json({eventId: uuid4()});
+});
+
+app.post('/users', (request, response) => {
+    return response.status(201).json({userId: uuid4()});
 });
 
 module.exports = app;
--- tests/server/app.test.js
+++ tests/server/app.test.js
@@ -1,12 +1,67 @@
 import request from 'supertest';
 import app from './../../src/server/app';
 
-describe('GET /events', function() {
-    it('respond with json', function(done) {
-        request(app)
-            .get('/events')
+describe('Get events', () => {
+    it('can get events for given user', () => {
+        return request(app)
+            .get('/events?userId=user123')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .then(response => {
+                expect(response.body.length).toEqual(2)
+            })
+    });
+
+    it('can not get events for unauthorized user', () => {
+        return request(app)
+            .get('/events?userId=unknown')
+            .set('Accept', 'application/json')
+            .expect(403);
+    });
+});
+
+describe('Add events', () => {
+    it('can add event for given user', () => {
+        return request(app)
+            .post('/events?userId=user123')
+            .send({
+                start: '2017-01-01',
+                end: '2017-01-02',
+                event: "Quick event 1",
+                allDay: false,
+            })
+            .set('Accept', 'application/json')
+            .expect(201)
+            .then(response => {
+                expect(response.body.eventId.length).toEqual(36)
+            })
+    });
+
+    it('can not add events for unauthorized user', () => {
+        return request(app)
+            .post('/events?userId=unknown')
+            .set('Accept', 'application/json')
+            .expect(403);
+    });
+
+    it('can not add event with invalid input', () => {
+        return request(app)
+            .post('/events?userId=user123')
+            .send('name=john')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
-            .expect(200, done);
+            .expect(400);
+    });
+});
+
+describe('User', () => {
+    it('can get userId', () => {
+        return request(app)
+            .post('/users')
+            .set('Accept', 'application/json')
+            .expect(201)
+            .then(response => {
+                expect(response.body.userId.length).toEqual(36)
+            })
     });
 });
