3c822a2ce8757199842bec71f28166bc4b66cfa5
lzakrzewski <lucaszz111@gmail.com>
2018-09-20 13:55:31 +0100

    Validation middleware implementation

--- src/server/middleware/validationMiddleware.js
+++ src/server/middleware/validationMiddleware.js
@@ -1,7 +1,7 @@
 export const eventValidation = (request, response, next) => {
     const event = request.body;
 
-    if (!event.start || !event.event) {
+    if (!event.start || !event.event || !event.end) {
         return response.status(400).json({error: 'Bad request'});
     }
 
--- tests/server/unit/middleware/validationMiddleware.test.js
+++ tests/server/unit/middleware/validationMiddleware.test.js
@@ -1,25 +1,58 @@
-import { eventValidation } from '../../../../src/server/middleware/validationMiddleware';
+import {eventValidation} from '../../../../src/server/middleware/validationMiddleware';
 
 describe('Validate events', () => {
+    let response = null;
+    let next = null;
+
+    beforeEach(() => {
+        next = jest.fn();
+
+        response = {
+            status: (status) => {
+                return {
+                    json: () => status
+                };
+            },
+        };
+    });
+
     it('calls next when event is valid', async () => {
         const request = {
             body: {
                 start: '2017-01-01',
                 end: '2017-01-02',
-                allDay: true,
+                event: 'some event name',
             }
         };
 
-        let response = {
-            status: () => {
-                return {
-                    json: () => 'ok'
-                };
-            },
+        eventValidation(request, response, next);
+
+        expect(next.mock.calls.length).toBe(1);
+    });
+
+    it('can not call next when event is invalid', async () => {
+        const request = {
+            body: {
+                start: '2017-01-01',
+                event: 'some event name',
+            }
         };
 
-        eventValidation(request, response, () => {
-            expect(1).toEqual(1);
-        });
+        eventValidation(request, response, next);
+
+        expect(next.mock.calls.length).toBe(0);
+    });
+
+    it('returns bad request response when even is invalid', async () => {
+        const request = {
+            body: {
+                start: '2017-01-01',
+                end: '2017-01-02',
+            }
+        };
+
+        const result = eventValidation(request, response, next);
+
+        expect(result).toEqual(400);
     });
 });
