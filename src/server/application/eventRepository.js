b6c34c2df334a95a45e6cb03795dbfca78b4cce4
lzakrzewski <lucaszz111@gmail.com>
2018-11-11 20:39:31 +0000

    mongodb credentials

--- src/server/application/eventRepository.js
+++ src/server/application/eventRepository.js
@@ -2,7 +2,7 @@ import moment from 'moment';
 import mongodb from 'mongodb';
 import _ from 'lodash';
 
-const url = 'mongodb://127.0.0.1:27017/';
+const url = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/';
 const mongoClient = mongodb.MongoClient;
 
 let connection = null;
